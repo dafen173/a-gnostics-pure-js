c7d3a601004500fe06fe49b70575f592e8270780
Alex <dafen@ukr.net>
2023-09-04 22:22:43 +0300

    feat: add navbar items for separate pages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -57,4 +57,40 @@ document.addEventListener('click', function handleClickOutsideBox(event) {
             showMenu = false
         }
     }
-});
+})
+
+
+
+//==================== EVENT DELEGATION IN DROP DOWN MENU =======================================
+
+// document.querySelector('.services_page_test').addEventListener('click', function(e) {
+//     // window.location.href = 'http://example.com';
+//     console.log(e.target)
+// }, false)
+
+
+// table.onclick = function(event) {
+//     let td = event.target.closest('td'); // (1)
+
+//     if (!td) return; // (2)
+
+//     if (!table.contains(td)) return; // (3)
+
+//     highlight(td); // (4)
+// };
+
+
+// const subMenuTarget = document.querySelector('.header__navbar__item__submenu')
+
+// navbarLinks.addEventListener('click', function(event) {
+//     console.log(event.target.classList.contains('header__navbar__item__submenu--diagnostic'))
+
+//     if (event.target.classList.contains('header__navbar__item__submenu--diagnostic')) {
+//         window.location.href = './product-di-agnostics.html'
+//     }
+
+//     if (event.target === subMenuTarget) {
+//         console.log('yoooooo')
+//     }
+// })
+
